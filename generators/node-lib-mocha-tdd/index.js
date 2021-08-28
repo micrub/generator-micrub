e70bb36eee43ebd832d0cc7c523bee3f415b4a7c
Michael Rubanov <michael@lusha.com>
2021-08-28 19:37:17 +0300

    feat(subgenerator): node.js library project generator
    Instrumented with mocha and chai for TDD.

--- generators/node-lib-mocha-tdd/index.js
+++ generators/node-lib-mocha-tdd/index.js
@@ -1,23 +1,25 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the groovy ${chalk.red('generator-micrub')} generator!`
+        `Welcome to the groovy ${chalk.red(
+          "generator-micrub"
+        )} node-lib-tdd generator!`
       )
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
       }
     ];
 
@@ -28,13 +30,44 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    const copyList = [
+      "index.js",
+      "utils.js",
+      "_mocharc.js",
+      "_gitignore",
+      "test/"
+    ];
+    const templateList = ["package.json", "package-lock.json"];
+    for (let index = 0; index < copyList.length; index++) {
+      let copyPathFrom = copyList[index];
+      let copyPathTo = copyPathFrom;
+
+      // Handle _ prefix
+      if (copyPathFrom.charAt(0) === "_") {
+        copyPathTo = copyPathFrom.replace("_", ".");
+      }
+
+      this.fs.copy(
+        this.templatePath(copyPathFrom),
+        this.destinationPath(copyPathTo)
+      );
+    }
+
+    for (let index = 0; index < templateList.length; index++) {
+      const copyPathFrom = templateList[index];
+      const copyPathTo = copyPathFrom;
+
+      this.fs.copyTpl(
+        this.templatePath(copyPathFrom),
+        this.destinationPath(copyPathTo),
+        { ...this.props }
+      );
+    }
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false
+    });
   }
 };
---
+++ generators/node-lib-mocha-tdd/templates/test/global-setup.js
@@ -0,0 +1,5 @@
+const { d } = require('../utils');
+
+module.exports = async () => {
+  require('chai/register-expect');  // Using Expect style;
+};
